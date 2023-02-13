71c8cae70b5da966e8716d30c250b15787228fcc
PaneruVipin <paneruvipinbusiness@gmail.com>
2023-02-13 14:15:08 +0530

    miner

--- start/routes.ts
+++ start/routes.ts
@@ -20,24 +20,33 @@
 
 import Route from "@ioc:Adonis/Core/Route";
 import Application from "@ioc:Adonis/Core/Application";
+import Env from "@ioc:Adonis/Core/Env";
+
 // import { extname } from 'path'
 /**
- * 
+ *
  */
 Route.get("/", async () => {
   return { hello: "world" };
 });
 
 Route.post("post/*", async ({ request }) => {
-  const params=request.params() as object
-  const paramURL=params?.["*"]?.join("/")
+  const params = request.params() as object;
+  const paramURL = params?.["*"]?.join("/");
   const file = request.file("file");
-  const name=new Date().getTime()+ "."+file?.extname
-  await file?.move(Application.tmpPath("uploads/"+paramURL), {
+  const name = new Date().getTime() + "." + file?.extname;
+  await file?.move(Application.tmpPath("uploads/" + paramURL), {
     name,
     overwrite: true,
   });
-  return {URL:"https://vide0-uploader.onrender.com/uploads/"+paramURL+"/"+name}
+  if (Env.get("NODE_ENV") == "development") {
+    return { URL: Application.tmpPath("uploads/" + paramURL + "/" + name) };
+  } else {
+    return {
+      URL:
+        "https://vide0-uploader.onrender.com/uploads/" + paramURL + "/" + name,
+    };
+  }
 });
 
 // Route.post("download", async ({ params, response }) => {
@@ -49,5 +58,3 @@ Route.post("post/*", async ({ request }) => {
 //   // console.log(Application.tmpPath());
 //   // return "File does not exist";
 // });
-
-
